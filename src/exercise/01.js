52f0e233758b930687aad2fd40f34e01a1b2ae8b
郑震 <2693781894@qq.com>
2024-01-12 23:14:21 +0800

    exercise 1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -5,10 +5,12 @@ import * as React from 'react'
 
 function Greeting() {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  const [name,SetName] = React.useState('')
+
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    SetName(event.target.value)
   }
 
   return (
